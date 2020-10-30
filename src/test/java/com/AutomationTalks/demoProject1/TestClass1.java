ec6583eb54690bd58d7c3196ab013a7d429e0674
Manoj Karia <kariamanoj@icloud.com>
2020-10-30 12:40:20 -0400

    setup selenium grid

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -1,9 +1,15 @@
 package com.AutomationTalks.demoProject1;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,9 +19,20 @@ public class TestClass1 {
 public static WebDriver driver;
 	
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException{
+
+//		DesiredCapabilities dr = null;
+		ChromeOptions options = new ChromeOptions();
+		options.setCapability("setBrowserName", "chrome");
+		options.setCapability("setPlatform", Platform.WIN10);
+//		ChromeDriver dr = new ChromeDriver(options);
+//		dr = DesiredCapabilities.chrome();
+//		dr.setBrowserName("chrome");
+//		dr.setVersion("85.0.4183.87");
+//		dr.setPlatform(Platform.WIN10);
 		System.setProperty("webdriver.chrome.driver", "C:\\MK\\QA\\Automation\\Drivers\\chromedriver.exe"); 
-		driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL("http://192.168.0.16:4444/wd/hub"), options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
