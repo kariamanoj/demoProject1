a4172a20dbd5189b56132dbdeb61971008f529f1
Manoj Karia <kariamanoj@icloud.com>
2020-10-30 13:48:35 -0400

    setup browserstck

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -17,22 +17,35 @@ import org.testng.annotations.Test;
 public class TestClass1 {
 	
 public static WebDriver driver;
+public static final String USERNAME = "qatester163";
+public static final String AUTOMATE_KEY = "LYe9TbNzifwCC9zmBVbV";
+public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 	
 	@BeforeMethod
 	public void launchDriver() throws MalformedURLException{
+	
 
 //		DesiredCapabilities dr = null;
-		ChromeOptions options = new ChromeOptions();
-		options.setCapability("setBrowserName", "chrome");
-		options.setCapability("setPlatform", Platform.WIN10);
+		  DesiredCapabilities caps = new DesiredCapabilities();
+	        
+		  caps.setCapability("os", "Windows");
+		  caps.setCapability("os_version", "10");
+		  caps.setCapability("browser", "Chrome");
+		  caps.setCapability("browser_version", "80");
+		  caps.setCapability("name", "My First Test");
+		  
+//		ChromeOptions options = new ChromeOptions();
+//		options.setCapability("setBrowserName", "chrome");
+//		options.setCapability("setPlatform", Platform.WIN10);
 //		ChromeDriver dr = new ChromeDriver(options);
 //		dr = DesiredCapabilities.chrome();
 //		dr.setBrowserName("chrome");
 //		dr.setVersion("85.0.4183.87");
 //		dr.setPlatform(Platform.WIN10);
-		System.setProperty("webdriver.chrome.driver", "C:\\MK\\QA\\Automation\\Drivers\\chromedriver.exe"); 
+//		System.setProperty("webdriver.chrome.driver", "C:\\MK\\QA\\Automation\\Drivers\\chromedriver.exe"); 
 //		driver = new ChromeDriver();
-		driver = new RemoteWebDriver(new URL("http://192.168.0.16:4444/wd/hub"), options);
+//		driver = new RemoteWebDriver(new URL("http://192.168.0.16:4444/wd/hub"), options);
+		driver = new RemoteWebDriver(new URL(URL), caps);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
