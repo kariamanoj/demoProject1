2d2c1755d5b03dd6711826d78691f48995fb1a81
Manoj Karia <kariamanoj@icloud.com>
2021-10-09 12:34:24 -0400

    Just Committing

--- src/main/java/com/AutomationTalks/demoProject1/public class App1.java
+++ src/main/java/com/AutomationTalks/demoProject1/public class App1.java
@@ -7,5 +7,6 @@ public class App1
         System.out.println( "Update on GIT );
         System.out.println( "Second Update on GIT );
         System.out.println( "Third Update on GIT );
+        System.out.println( "Update here);
     }
 }
