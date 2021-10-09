93ebff7635d6b0c2a59a57d6a54bf58e8d0f83b4
Manoj Karia <kariamanoj@icloud.com>
2021-10-09 12:15:25 -0400

    Second commit today

--- src/main/java/com/AutomationTalks/demoProject1/App.java
+++ src/main/java/com/AutomationTalks/demoProject1/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! I am back" );
+        System.out.println( "What's Up Friends" );
     }
 }
---
+++ src/main/java/com/AutomationTalks/demoProject1/public class App1.java
@@ -0,0 +1,8 @@
+public class App1 
+{
+    public justprint()
+    {
+        System.out.println( "Second Class" );
+        System.out.println( "Just Checking );
+    }
+}
