6c6a84d7b941fe8c72a443fd0eb9f34a3cdf0e99
kariamanoj <59589084+kariamanoj@users.noreply.github.com>
2021-10-09 12:37:00 -0400

    Update public class App1.java

--- src/main/java/com/AutomationTalks/demoProject1/public class App1.java
+++ src/main/java/com/AutomationTalks/demoProject1/public class App1.java
@@ -7,10 +7,8 @@ public class App1
         System.out.println( "Update on GIT );
         System.out.println( "Second Update on GIT );
         System.out.println( "Third Update on GIT );
-<<<<<<< HEAD
         System.out.println( "Update here);
-=======
         System.out.println( "Fourth Update on GIT );
->>>>>>> 12529c1239e27bd6b2274f9b104b2a86c682fd9e
+
     }
 }
